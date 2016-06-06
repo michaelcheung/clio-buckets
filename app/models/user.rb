248b0a00569738981317eec6f96dc6883184d54c
Kyle d'Oliveira <kyle@goclio.com>
2016-06-06 09:26:50 -0700

    Add timeoutable module so that user sessions timeout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  devise :omniauthable, omniauth_providers: [:google_oauth2]
+  devise :omniauthable, :timeoutable, omniauth_providers: [:google_oauth2]
 
   validates :email, :full_name, presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { sessions: 'devise/sessions', omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { sessions: 'users/sessions', omniauth_callbacks: "users/omniauth_callbacks" }
 
   resources :departments, only: [:index] do
     resources :roles, only: [:index]
